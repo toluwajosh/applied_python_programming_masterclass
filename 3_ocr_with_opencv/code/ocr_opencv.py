89776949af6afad7ab1c7ed100f170300fd57c80
toluwajosh <tjosh.owoyemi@gmail.com>
2020-04-18 13:55:27 +0900

    update code for ocr with opencv

--- 3_ocr_with_opencv/code/ocr_opencv.py
+++ 3_ocr_with_opencv/code/ocr_opencv.py
@@ -5,7 +5,7 @@ ocr script
 import cv2
 import numpy as np
 
-input_image = cv2.imread("blank_paper.jpg")
+input_image = cv2.imread("afam_paper.jpg")
 gray_image = cv2.cvtColor(input_image, cv2.COLOR_RGB2GRAY)
 
 # detect paper edges
@@ -19,32 +19,47 @@ dst = cv2.cornerHarris(gray_image, 2, 3, 0.04)
 dst = cv2.dilate(dst, None)
 
 # Threshold for an optimal value, it may vary depending on the image.
-input_image[dst > 0.01 * dst.max()] = [0, 0, 255]
+# input_image[dst > 0.01 * dst.max()] = [0, 0, 255]
 
 
-# detect lines
-lines = cv2.HoughLines(edge_image, 1, np.pi / 90, 100)
-for rho, theta in lines[0]:
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a * rho
-    y0 = b * rho
-    x1 = int(x0 + 1000 * (-b))
-    y1 = int(y0 + 1000 * (a))
-    x2 = int(x0 - 1000 * (-b))
-    y2 = int(y0 - 1000 * (a))
+def get_paper_corners():
+    corners = []
 
-    cv2.line(input_image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    def get_click_coord(event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            if len(corners) > 4:
+                corners.pop()
+            corners.append([x, y])
+            print(corners)
+        elif event == cv2.EVENT_MBUTTONDOWN:
+            if len(corners) > 0:
+                corners.pop()
+                print(corners)
 
-# minLineLength = 100
-# maxLineGap = 100
-# lines = cv2.HoughLinesP(
-#     edge_image, 1, np.pi / 180, 200, minLineLength, maxLineGap
-# )
-# for x1, y1, x2, y2 in lines[0]:
-#     cv2.line(input_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    # give our window a name
+    cv2.namedWindow("display")
+    cv2.setMouseCallback("display", get_click_coord)
+
+    cv2.imshow("display", input_image)
+    cv2.waitKey(0)
+
+    # raise an error if the number of corners supplied is no up to 4
+    assert len(corners) > 3, "Corner points are not complete. Must be 4!"
+    return corners
+
+
+new_corners = np.float32([[0, 0], [540, 0], [0, 720], [540, 720]])
+print(input_image.shape)
+
+# run the get paper corners function
+paper_corners = np.float32(get_paper_corners())
+print(paper_corners)
+
+transform_matrix = cv2.getPerspectiveTransform(paper_corners, new_corners)
+warped_image = cv2.warpPerspective(input_image, transform_matrix, (540, 720))
 
 
 cv2.imshow("display", input_image)
+cv2.imshow("transformed", warped_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- 5_ocr_web_app/ocr_web_app.md
+++ 5_ocr_web_app/ocr_web_app.md
@@ -1,1 +1,7 @@
 # Create OCR Web Application
+
+
+# save image from webcam
+
+https://rk.edu.pl/en/capturing-images-webcam-django-jpegcam/
+https://django-webcam.readthedocs.io/en/latest/
